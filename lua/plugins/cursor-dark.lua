925dcd645b4913029688e0fd8a11e32fb5d3a742
Basian Lesi <basian.lesi@biie.ai>
2026-01-23 23:15:19 +0100

    fix cursor dark

--- lua/plugins/cursor-dark.lua
+++ lua/plugins/cursor-dark.lua
@@ -1,20 +1,17 @@
 return {
-  -- Your other plugins...
-
   {
     "ydkulks/cursor-dark.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      -- vim.cmd.colorscheme("cursor-dark-midnight")
       require("cursor-dark").setup({
-        -- For theme
-        style = "dark-midnight",
-        -- For a transparent background
-        transparent = true,
-        -- If you have dashboard-nvim plugin installed
+        style = "dark",
+        transparent = false,
         dashboard = true,
       })
+
+      -- APPLY the theme
+      vim.cmd.colorscheme("cursor-dark")
     end,
   },
 }
